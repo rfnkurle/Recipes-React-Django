ff959150352a0d85bdf82d1c9ca36bf12c6ff38e
rfnkurle <45407665+rfnkurle@users.noreply.github.com>
2024-07-14 16:20:57 -0600

    views post

--- server/recipes/views.py
+++ server/recipes/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http.response import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http.response import JsonResponse
+from .models import Recipe
+from .serializers import RecipeSerializer
 
-# Create your views here.
+
+class RecipeView(APIView):
+   
+    def post(self, request):
+        data = request.data
+        serializer = RecipeSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse("Recipe Added Successfully", safe=False)
+        return JsonResponse("Failed to Add Recipe", safe=False)
+    
+  
