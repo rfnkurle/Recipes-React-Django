0cbc7a57b3fa2959ed237104c80023a5527fbb1c
rfnkurle <45407665+rfnkurle@users.noreply.github.com>
2024-07-15 10:55:07 -0600

    migrations to adjest character length in model

--- server/recipes/models.py
+++ server/recipes/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Recipe(models.Model):
     recipeId = models.AutoField(primary_key=True)
     recipeName = models.CharField(max_length=100)
-    time = models.CharField(max_length=100)
-    ingredients = models.CharField(max_length=100)
-    instructions = models.CharField(max_length=100)
+    time = models.CharField(max_length=20)
+    ingredients = models.CharField(max_length=300)
+    instructions = models.CharField(max_length=400)
