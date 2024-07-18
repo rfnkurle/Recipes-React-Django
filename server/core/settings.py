cd77c37eeeb8c46f87195160ab99baef593f6359
rfnkurle <45407665+rfnkurle@users.noreply.github.com>
2024-07-18 14:00:28 -0600

    hosting

--- server/core/settings.py
+++ server/core/settings.py
@@ -130,4 +130,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:8000"] 
+CORS_ALLOWED_ORIGINS = ['http://localhost:8000', 'http://localhost:5173'] 
